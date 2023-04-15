8afda4bb9284619d5ab901812e6e63ef9e3c7b11
Vladislav <109065019+TimVladislav13010@users.noreply.github.com>
2023-04-15 19:16:14 +0300

    role

--- src/routes/contacts.py
+++ src/routes/contacts.py
@@ -4,15 +4,21 @@ from fastapi import Depends, HTTPException, status, Path, APIRouter, Query
 from sqlalchemy.orm import Session
 
 from src.database.db import get_db
-from src.database.models import User
+from src.database.models import User, Role
 from src.schemas import ContactModel, ContactResponse
 from src.repository import contacts as repository_contacts
 from src.services.auth import auth_service
+from src.services.roles import RoleAccess
 
 router = APIRouter(prefix="/contacts", tags=['contacts'])
 
+allowed_operation_get = RoleAccess([Role.admin, Role.moderator, Role.user])
+allowed_operation_create = RoleAccess([Role.admin, Role.moderator, Role.user])
+allowed_operation_update = RoleAccess([Role.admin, Role.moderator])
+allowed_operation_remove = RoleAccess([Role.admin])
 
-@router.get("/search", response_model=List[ContactResponse])
+
+@router.get("/search", response_model=List[ContactResponse], dependencies=[Depends(allowed_operation_get)])
 async def search_contacts(first_name: str = Query(None),
                           last_name: str = Query(None),
                           email: str = Query(None),
@@ -22,13 +28,13 @@ async def search_contacts(first_name: str = Query(None),
     return contacts
 
 
-@router.get("/birthday", response_model=List[ContactResponse])
+@router.get("/birthday", response_model=List[ContactResponse], dependencies=[Depends(allowed_operation_get)])
 async def birthday_contacts(current_user: User = Depends(auth_service.get_current_user), db: Session = Depends(get_db)):
     contacts = await repository_contacts.birthday_contacts(current_user, db)
     return contacts
 
 
-@router.get("/", response_model=List[ContactResponse])
+@router.get("/", response_model=List[ContactResponse], dependencies=[Depends(allowed_operation_get)])
 async def get_contacts(limit: int = Query(10, le=500), offset: int = 0,
                        current_user: User = Depends(auth_service.get_current_user),
                        db: Session = Depends(get_db)):
@@ -36,7 +42,7 @@ async def get_contacts(limit: int = Query(10, le=500), offset: int = 0,
     return contacts
 
 
-@router.get("/{contact_id}", response_model=ContactResponse)
+@router.get("/{contact_id}", response_model=ContactResponse, dependencies=[Depends(allowed_operation_get)])
 async def get_contact(contact_id: int = Path(ge=1),
                       current_user: User = Depends(auth_service.get_current_user),
                       db: Session = Depends(get_db)):
@@ -46,7 +52,9 @@ async def get_contact(contact_id: int = Path(ge=1),
     return contact
 
 
-@router.post("/", response_model=ContactResponse, status_code=status.HTTP_201_CREATED)
+@router.post("/", response_model=ContactResponse,
+             status_code=status.HTTP_201_CREATED,
+             dependencies=[Depends(allowed_operation_create)])
 async def create_contact(body: ContactModel,
                          current_user: User = Depends(auth_service.get_current_user),
                          db: Session = Depends(get_db)):
@@ -54,7 +62,7 @@ async def create_contact(body: ContactModel,
     return contact
 
 
-@router.put("/{contact_id}", response_model=ContactResponse)
+@router.put("/{contact_id}", response_model=ContactResponse, dependencies=[Depends(allowed_operation_update)])
 async def update_contact(body: ContactModel,
                          contact_id: int = Path(ge=1),
                          current_user: User = Depends(auth_service.get_current_user),
@@ -65,7 +73,7 @@ async def update_contact(body: ContactModel,
     return contact
 
 
-@router.delete("/{contact_id}", status_code=status.HTTP_204_NO_CONTENT)
+@router.delete("/{contact_id}", status_code=status.HTTP_204_NO_CONTENT, dependencies=[Depends(allowed_operation_remove)])
 async def remove_contact(contact_id: int = Path(ge=1),
                          current_user: User = Depends(auth_service.get_current_user),
                          db: Session = Depends(get_db)):
